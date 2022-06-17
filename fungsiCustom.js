934908baeed5878d96bd50514257e51a2affcd81
nabilahdyh <nabilahidayah.nh@gmail.com>
2022-06-17 23:15:39 +0700

    Chall

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -18,7 +18,37 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let temp = [];
+  fs.readFile(file2, "utf8", (err, data) => {
+    if (err) fnCallback(err, null);
+    temp.push(jsonType2(data));
+    fs.readFile(file1, "utf8", (err, data) => {
+      if (err) fnCallback(err, null);
+      temp.push(jsonType1(data));
+      fs.readFile(file3, "utf8", (err, data) => {
+        if (err) fnCallback(err, null);
+        temp.push(jsonType3(data));
+
+        fnCallback(null, temp);
+      });
+    });
+  });
+};
+const jsonType1 = (j) => {
+  let obj = JSON.parse(j);
+  return obj.message.split(" ")[1];
+};
+
+const jsonType2 = (j) => {
+  let obj = JSON.parse(j);
+  return obj[0].message.split(" ")[1];
+};
+
+const jsonType3 = (j) => {
+  let obj = JSON.parse(j);
+  return obj[0].data.message.split(" ")[1];
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
